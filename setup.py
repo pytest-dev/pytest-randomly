a0ca4967d16cefd1ae6881df68c89b04f3c62edd
Adam Chainz <adam@adamj.eu>
2016-04-16 10:04:58 +0100

    setup.py - add Pytest trove classifier

--- setup.py
+++ setup.py
@@ -63,10 +63,11 @@ setup(
     },
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
+        'Framework :: Pytest',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
