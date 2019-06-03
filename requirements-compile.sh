2d1e9c1b5c8aad5ae7b0f192059cf3b695ce456f
Adam Johnson <me@adamj.eu>
2019-06-03 22:14:30 +0100

    Add generated hashes to requirements files (#185)
    * Move all requirements files under `requirements/`
    * Add `--generate-hashes` to `compile.sh` and rerun
    * Upgrade requirements whilst at it

--- requirements-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-set -ex
-export CUSTOM_COMPILE_COMMAND="./requirements-compile.sh"
-python3.5 -m piptools compile "$@" -o requirements/py35.txt
-python3.6 -m piptools compile "$@" -o requirements/py36.txt
-python3.7 -m piptools compile "$@" -o requirements/py37.txt
