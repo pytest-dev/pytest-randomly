4206e9e2d0813c44de79639a13eaa87e940a4d1a
Adam Johnson <me@adamj.eu>
2019-11-11 21:24:42 +0000

    Test with Python 3.8

--- requirements/compile.sh
+++ requirements/compile.sh
@@ -4,3 +4,4 @@ export CUSTOM_COMPILE_COMMAND="./compile.sh"
 python3.5 -m piptools compile --generate-hashes "$@" -o py35.txt
 python3.6 -m piptools compile --generate-hashes "$@" -o py36.txt
 python3.7 -m piptools compile --generate-hashes "$@" -o py37.txt
+python3.8 -m piptools compile --generate-hashes "$@" -o py38.txt
