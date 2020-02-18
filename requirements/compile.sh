703f2e03d7fcee6fe6a14126ca3c9b52448254fe
Adam Johnson <me@adamj.eu>
2020-02-18 18:08:04 +0000

    Convert requirements compile script to Python (#229)
    This is to make it work whichever directory it's run from - something that's hard in a shell script.

--- requirements/compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -ex
-export CUSTOM_COMPILE_COMMAND="./compile.sh"
-python3.5 -m piptools compile --generate-hashes "$@" -o py35.txt
-python3.6 -m piptools compile --generate-hashes "$@" -o py36.txt
-python3.7 -m piptools compile --generate-hashes "$@" -o py37.txt
-python3.8 -m piptools compile --generate-hashes "$@" -o py38.txt
