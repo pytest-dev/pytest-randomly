72e5c71e8b9364f3a93bf574c13154d12342e041
Adam Johnson <me@adamj.eu>
2021-05-06 13:36:38 +0100

    Support Python 3.10.

--- src/pytest_randomly.py
+++ src/pytest_randomly.py
@@ -1,10 +1,14 @@
 import argparse
 import hashlib
 import random
+import sys
 
-# if sys.version_info < (3, 10):
-from importlib_metadata import entry_points
 from pytest import Collector, fixture
+
+if sys.version_info < (3, 10):
+    from importlib_metadata import entry_points
+else:
+    from importlib.metadata import entry_points
 
 try:
     import xdist
